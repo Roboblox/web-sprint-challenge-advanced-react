901a827eada210ecced1c30d0836be38665028fd
roboblox <supeecyborg@gmail.com>
2020-08-07 18:01:40 -0400

    finished

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
-
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
+import useForm from "../hooks/useForm/";
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
+  const [
+    showSuccessMessage,
+    setShowSuccessMessage,
+    values,
+    setValues,
+    handleChanges,
+  ] = useForm();
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
@@ -66,7 +62,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button role="button">Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,43 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  const { getByText, getByRole, getByTestId } = render(<CheckoutForm />);
+  const header = getByText(/checkout form/i);
+  // Assert that the header is displayed
+  // I expect the header to be in the document
+  expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  const { getByText, getByRole, getByTestId, getByLabelText } = render(
+    <CheckoutForm />
+  );
+  const firstNameInput = getByLabelText(/First Name:/i);
+  const lastNameInput = getByLabelText(/Last Name:/i);
+  const addressInput = getByLabelText(/Address:/i);
+  const cityInput = getByLabelText(/City:/i);
+  const stateInput = getByLabelText(/State:/i);
+  const zipInput = getByLabelText(/Zip:/i);
+  fireEvent.change(firstNameInput, { target: { value: "aa" } });
+  fireEvent.change(lastNameInput, { target: { value: "bb" } });
+  fireEvent.change(addressInput, { target: { value: "cc" } });
+  fireEvent.change(cityInput, { target: { value: "zz" } });
+  fireEvent.change(stateInput, { target: { value: "yye" } });
+  fireEvent.change(zipInput, { target: { value: "ff" } });
+
+  const submitButton = getByRole("button");
+  fireEvent.click(submitButton);
+  const successMessage = getByTestId("successMessage");
+  expect(successMessage).toBeVisible();
+  //   expect(firstNameInput.textContent).toBe(/ming/i);
+  expect(getByText(/aa/i)).toBeInTheDocument();
+  expect(getByText(/bb/i)).toBeInTheDocument();
+  expect(getByText(/cc/i)).toBeInTheDocument();
+  expect(getByText(/zz/i)).toBeInTheDocument();
+  expect(getByText(/yye/i)).toBeInTheDocument();
+  expect(getByText(/ff/i)).toBeInTheDocument();
+});
